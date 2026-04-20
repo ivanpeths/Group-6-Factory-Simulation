b20f0c6308eceac0ca1841c9af96c049c9b3c2c8
kolbyng <kolbyng@gmail.com>
2026-04-20 13:10:52 -0400

    added progress bars and starting timer to game world

--- FactoryWorld.java
+++ FactoryWorld.java
@@ -10,6 +10,8 @@ public class FactoryWorld extends World
 {
     private GreenfootImage background;
     
+    private boolean started = false;
+    
     // Score variables
     private Label leftScoreLabel;
     private Label rightScoreLabel;
@@ -17,6 +19,17 @@ public class FactoryWorld extends World
     private int rightScore;
     private int labelY = 75;
     private int labelSize = 50;
+    
+    // Bar variables
+    private SuperStatBar leftBar;
+    private SuperStatBar rightBar;
+    private int leftBarProgress = 180;
+    private int rightBarProgress = 0;
+    private int barHeight = 40;
+    
+    // Starting timer variables;
+    private int startingTimer = 180;
+    
     /**
      * Factory World constructor. Add parameters if needed
      */
@@ -26,6 +39,7 @@ public class FactoryWorld extends World
         
         setBackground();
         drawLabels(1, 1);
+        drawBars();
         
     }
     
@@ -42,5 +56,29 @@ public class FactoryWorld extends World
         rightScoreLabel = new Label("$" + leftStarting + "/$100", labelSize);
         rightScore = rightStarting;
         addObject(rightScoreLabel, getWidth() / 8 * 6, labelY);
+    }
+    
+    public void drawBars(){
+        leftBar = new SuperStatBar(leftBarProgress, 0, null, getWidth() / 2, barHeight, 0, Color.GREEN, Color.GRAY);
+        rightBar = new SuperStatBar(rightBarProgress, 180, null, getWidth() / 2, barHeight, 0, Color.GREEN, Color.GRAY);
+        addObject(leftBar, 0, 0);
+        addObject(rightBar, getWidth() / 2, 0);
+    }
+    
+    public void updateBars(){
+        leftBarProgress--;
+        rightBarProgress--;
+        leftBar.update(leftBarProgress);
+        rightBar.update(rightBarProgress);
+    }
+    
+    public void act(){
+        if (!started){
+            startingTimer--;
+            if (startingTimer <=0){
+                
+            }
+            updateBars();
+        }
     }
 }
