c2c6ed4509c57f766dd195b468155d5ec3a9f70f
kolbyng <kolbyng@gmail.com>
2026-04-20 12:54:47 -0400

    added superstatbar, drawlabels in world

--- FactoryWorld.java
+++ FactoryWorld.java
@@ -10,15 +10,37 @@ public class FactoryWorld extends World
 {
     private GreenfootImage background;
     
+    // Score variables
+    private Label leftScoreLabel;
+    private Label rightScoreLabel;
+    private int leftScore;
+    private int rightScore;
+    private int labelY = 75;
+    private int labelSize = 50;
     /**
-     * Constructor for objects of class MyWorld.
+     * Factory World constructor. Add parameters if needed
      */
     public FactoryWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1); 
         
+        setBackground();
+        drawLabels(1, 1);
+        
+    }
+    
+    public void setBackground(){
         background = new GreenfootImage ("background.png");
         setBackground (background);
+    }
+    
+    public void drawLabels(int leftStarting, int rightStarting){
+        leftScoreLabel = new Label("$" + leftStarting + "/$100", labelSize);
+        leftScore = leftStarting;
+        addObject(leftScoreLabel, getWidth() / 8 * 2, labelY);
+        
+        rightScoreLabel = new Label("$" + leftStarting + "/$100", labelSize);
+        rightScore = rightStarting;
+        addObject(rightScoreLabel, getWidth() / 8 * 6, labelY);
     }
 }
