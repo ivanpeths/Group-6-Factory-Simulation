5958e27a2a4ad731cd2b10e1ebb0f526515f8ec5
Clone5030 <440021056@gapps.yrdsb.ca>
2026-04-24 13:24:43 -0400

    buttons in settings

--- SettingsWorld.java
+++ SettingsWorld.java
@@ -9,9 +9,28 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class SettingsWorld extends World
 {
     
+    private GreenfootImage buttonImg;
+    private Actor buttonActor;
+    private Label buttonTitle;
     public SettingsWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1); 
+        setupButton();
+        
+    }
+    
+    public void setupButton(){
+        buttonImg = new GreenfootImage("button.png");
+        buttonActor = new BlankActor();
+        buttonActor.setImage(buttonImg);
+        buttonTitle = new Label("Start", 75);
+        addObject(buttonActor, getWidth() / 2, getHeight() / 8 * 7);
+        addObject(buttonTitle, getWidth() / 2, getHeight() / 8 * 7 - 10);
+    }
+    
+    public void act(){
+        if(Greenfoot.mouseClicked(buttonActor) || Greenfoot.mouseClicked(buttonTitle)){
+            Greenfoot.setWorld(new FactoryWorld());
+        }
     }
 }
