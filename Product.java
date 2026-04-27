74dbf8aa63423ce0e32dc9d7899d37e5a30189f1
ivan440013993 <440013993@gapps.yrdsb.ca>
2026-04-27 13:25:10 -0400

    completely revamped product class to fit new objective

--- Product.java
+++ Product.java
@@ -1,5 +1,3 @@
-// Product made by Isaac, updated to an abstract superclass by Ivan
-
 import greenfoot.*;
 
 public abstract class Product extends SuperSmoothMover  
@@ -9,15 +7,19 @@ public abstract class Product extends SuperSmoothMover
 
     protected int type;
     protected GreenfootImage image;
+    protected int transparency;
+    
+    protected boolean inMachine;
 
     protected SimpleTimer spawnTimer = new SimpleTimer();
-    protected boolean processed = false;
 
     public Product(int owner)
     {
         this.owner = owner;
-        type = 0; // material
+        type = 1; // material
         updateImage();
+        inMachine = false;
+        transparency = 255;
     }
 
     public void act()
@@ -29,38 +31,19 @@ public abstract class Product extends SuperSmoothMover
             return;
         }
         checkLeavingMachine();
+        updateImage();
     }
 
     public void updateImage() 
     {
-        if (type == 0) image = new GreenfootImage("material.png");
-        else if (type == 1) image = new GreenfootImage("finishBox.png");
-        else if (type == 2) image = new GreenfootImage("brokenBox.png");
-        else if (type == 3) image = new GreenfootImage("expensiveBox.png");
+        if (type == 0) image = new GreenfootImage("brokenBox.png");
+        else if (type == 1) image = new GreenfootImage("material.png");
+        else if (type == 2) image = new GreenfootImage("finishBox.png");
+        else if (type >= 3) image = new GreenfootImage("expensiveBox.png");
         
         image.scale(50, 50);
         setImage(image);
-    }
-
-    public void setType(int newType) 
-    {
-        type = newType;
-        updateImage();
-    }
-
-    public void process() 
-    {
-        int rand = Greenfoot.getRandomNumber(100);
-
-        if (rand < 25) {
-            setType(2); // broken
-        } 
-        else if (rand < 95) {
-            setType(1); // finished
-        } 
-        else {
-            setType(3); // expensive
-        }
+        getImage().setTransparency(transparency);
     }
 
     public void moveDown()
@@ -72,22 +55,24 @@ public abstract class Product extends SuperSmoothMover
     {
         Machines m = (Machines)getOneIntersectingObject(Machines.class);
         
-        if (m != null && type == 0 && !processed && spawnTimer.millisElapsed() > 300)
-        {
-            if (!((Assembler)m).getBroken()) {
-                process();
-                processed = true;
-                ((Assembler)m).checkBreak();
+        if (m != null) {
+            if (!((Assembler)m).getBroken() && type != 0) {
+                inMachine = true;
+                transparency = 0;
             }
-            getImage().setTransparency(0);
         }
     }
     
     private void checkLeavingMachine() {
         Machines m = (Machines)getOneIntersectingObject(Machines.class);
         
-        if (m == null) {
-            getImage().setTransparency(255);
+        if (m == null && inMachine == true) {
+            type++;
+            if (Greenfoot.getRandomNumber(10) == 0) {
+                type = 0;
+            }
+            transparency = 255;
+            inMachine = false;
         }
     }
     
