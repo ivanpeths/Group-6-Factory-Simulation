c5a207f1d00bec6fd598f39f7985a189e11d7dc5
Clone5030 <440021056@gapps.yrdsb.ca>
2026-04-27 17:05:25 -0400

    resize pointer

--- Pointer.java
+++ Pointer.java
@@ -8,8 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Pointer extends SuperSmoothMover
 {
-    private int xSize = 50;
-    private int ySize = 75;
+    private int xSize = 30;
+    private int ySize = 50;
     
     public Pointer(){
         GreenfootImage img = new GreenfootImage("pointer.png");
