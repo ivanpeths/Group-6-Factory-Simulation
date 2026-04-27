4ee68d201df41887168d5f2240bd5abc857d0440
Clone5030 <440021056@gapps.yrdsb.ca>
2026-04-27 17:35:55 -0400

    added ambience steam sound effect

--- FactoryWorld.java
+++ FactoryWorld.java
@@ -80,12 +80,14 @@ public class FactoryWorld extends World
             soundMan.playStarting();
         } else {
             soundMan.playBgm();
+            soundMan.playAmbience();
         }
     }
     
     public void stopped(){
         soundMan.pauseStarting();
         soundMan.pauseBgm();
+        soundMan.pauseAmbience();
     }
     
     public void setXPos(int leftPos, int rightPos){
@@ -212,6 +214,7 @@ public class FactoryWorld extends World
                 removeCountdown();
                 drawTimer();
                 soundMan.playBgm();
+                soundMan.playAmbience();
             }
             updateCountdown();
             return;
