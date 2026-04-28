e3c5678a88dfab6dc7c39c7406bd20d836872d15
348995291-svg <348995291@gapps.yrdsb.ca>
2026-04-28 17:36:53 -0400

    Update Break.java

--- Break.java
+++ Break.java
@@ -4,7 +4,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Write a description of class BreakMachine here.
  * 
  * @author (your name) 
- * @version (a version number or a date)
+ * @version (a version number or a date) 
  */
 public class Break extends Upgrades
 {
