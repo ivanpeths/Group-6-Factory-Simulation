0cf9653cab57d758cfd96935f14dc2109a5f472a
Isaac Law <348995291@gapps.yrdsb.ca>
2026-04-28 08:58:28 -0400

    Copy from Metal. Changed it to Wood

--- Wood.java
+++ Wood.java
@@ -1,29 +1,29 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class Wood here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Wood extends Product
 {
     public Wood (int owner, double speed) {
         super(owner, speed);
     }
-    
-    public void sell () {
-        if (type == 0) {
-            addScore(5);
-        } else if (type == 1) {
-            addScore(10);
-        } else if (type == 2) {
-            addScore(0);
-        } else if (type == 3) {
-            addScore(15);
-        }
-    }
-    
+
     @Override
-    public void updateImage() {return;}
+    public void updateImage() 
+    {
+        if (type == 0) image = new GreenfootImage("placeholder.png");
+        else if (type == 1) image = new GreenfootImage("placeholder.png");
+        else if (type == 2) image = new GreenfootImage("placeholder.png");
+        else image = new GreenfootImage("placeholder.png");
+
+        image.scale(50, 50);
+        setImage(image);
+    }
+
+    @Override
+    public void sell() 
+    {
+        if (type == 0) addScore(5);
+        else if (type == 1) addScore(10);
+        else if (type == 2) addScore(20);
+        else if (type == 3) addScore(40);
+    }
 }
